a5809281f7bed3a5f34f64823368e3c4347afdb2
Guillermo Lezcano <guillermolezcanopy@gmail.com>
2026-06-22 21:22:53 -0300

    Comment out SocialProof component to temporarily disable it

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <main>
         <Hero />
         <Results />
-        <SocialProof />
+        {/* <SocialProof /> */}
         <Problem />
         <Solution />
         <HowItWorks />
